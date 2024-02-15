82b553c9cfe214e4f01d3f863981e4668a3db8cf
Nicolas-BZRD <nicolas.bzrd@gmail.com>
2024-02-15 17:07:56 +0100

    anonymous

--- policies/__init__.py
+++ policies/__init__.py
@@ -1,6 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
 from policies.mixed_precision import *
 from policies.wrapping import *
 from policies.activation_checkpointing_functions import apply_fsdp_checkpointing
